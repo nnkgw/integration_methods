9b4a6721ae6fbb829486c97d6ac5df2ab379ee52
nnkgw <nkgw@s241.xrea.com>
2012-10-21 18:30:55 +0900

    change spring simulation and osx binary

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,14 @@
 #include <string.h>
 
 enum eMethod{
-  eIdeal,
   eEuler,
   eVerlet,
 
   eNum,
 };
 
-struct sVector2{
-  double x;
-  double y;
-};
-
 int main (int argc, const char * argv[]) {
-  eMethod method   = eIdeal;
+  eMethod method   = eEuler;
   int     step_num = 60;
   if (argc < 2){ // parse args
     printf("usage:integration verlet 60\n");
@@ -26,55 +20,40 @@ int main (int argc, const char * argv[]) {
     if (strncmp(argv[1], "verlet", 6) == 0) { method = eVerlet; }
     sscanf(argv[2], "%d", &step_num);
   }
-  double dt = 1.0 / (double(step_num) / 10.0);
+  double dt = 1.0 / (double(step_num) / 4.0);
+  double t  = 0.0;
+  double x  = 0.0;
+  double a  = 0.0;
+  double v  = 0.0;
+  double m  = 10.0;
+  double k  = 1000.0;
+  double sx = 1.0;
+  double f  = 0.0f; // F = k(x-sx)
 
-  sVector2 position = {  0.0,  0.0 };
-  sVector2 velocity = { 10.0, 49.0 };
-
-  sVector2 prev_position;
-  sVector2 prev_velocity;
-  sVector2 next_position;
-  sVector2 next_velocity;
-
-  double t =  0.0;
-  double g = -9.8;
-
-  printf("# time position.y\n");
-  printf("%f %f\n", t, position.y);
+  printf("# time position\n");
+  //  printf("%f %f\n", t, x);
   switch(method) {
-  case eIdeal:
-       for(int i = 0; i < step_num; i++) {
-         t += dt;
-         position.x = velocity.x * t;
-         position.y = velocity.y * t + 0.5 * g * (t*t);
-         printf("%f %f\n", t, position.y);
-       }
-       break;
   case eEuler:
-       for(int i = 0; i < step_num; i++) {
-         t += dt;
-         velocity.x  = velocity.x;
-         velocity.y += g * dt;
-         position.x += velocity.x * dt;
-         position.y += velocity.y * dt;
-         printf("%f %f\n", t, position.y);
-       }       
-       break;
+    for(int i = 0; i < step_num; i++) {
+      f = -k * (x - sx);
+      a = f / m;
+      x += v * dt;
+      v += a * dt;
+      printf("%f %f\n", t, x);
+      t += dt;
+    }       
+    break;
   case eVerlet:
-       for(int i = 0; i < step_num; i++) {
-         t += dt;
-         if (i ==0) {
-           velocity.y += g * dt;
-           next_position.x = position.x + velocity.x * dt; // first step
-           next_position.y = position.y + velocity.y * dt;
-         }
-         prev_position = position;
-         position = next_position;
-         next_position.x = 2.0 * position.x - prev_position.x;
-         next_position.y = 2.0 * position.y - prev_position.y + g * (dt * dt);
-         printf("%f %f\n", t, position.y);
-       }
-       break;
+    double prev_x = 0.0;
+    for(int i = 0; i < step_num; i++) {
+      f = -k * (x - sx);
+      a = f / m;
+      x += (x - prev_x) + a * dt * dt;
+      prev_x = x;
+      printf("%f %f\n", t, x);
+      t += dt;
+     }
+     break;
   }
   return 0;
 }
